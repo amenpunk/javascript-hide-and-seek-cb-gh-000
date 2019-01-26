c3509730505b5e70a2b669a9daf8556ea645e8ed
Edgar Rolando Cosajay Campos <ondasycircuitos@gmail.com>
2019-01-26 00:47:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(''+selector);
+}
+function nestedTarget(){
+  return document.querySelector('div#nested div div div div.target');
+}
+function deepestChild(){
+  var child = document.getElementById('grand-node').querySelectorAll('div');
+  var deepest;
+  for(let i = 0; i<child.length-1; i++){
+    deepest = child[i].querySelector('div');
+  }
+  return deepest;
+}
+function increaseRankBy(n){
+  let child = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for(let i = 0; i<child.length; i++){
+    child[i].innerHTML = parseInt(child[i].innerHTML)+n;
+  }
+}
